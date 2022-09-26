fd598b173fbb5ba49f10650ae6b6f4404d5167c3
danieldamian09 <humbertorivero9@gmail.com>
2022-09-26 16:13:16 -0300

    Add dispatch y useSelector en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
+import { incrementar } from './store/slices/Contador/contadorSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const value = useAppSelector(state => state.contador.value)
+
+  const dispatch = useAppDispatch()
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {dispatch(incrementar())}}>
+          count is {value}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
